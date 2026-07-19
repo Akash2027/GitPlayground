900f34819229b8252bf8f02295a800691e6d1521
Akash K <akash260704@gmail.com>
2026-07-19 20:38:33 +0530

    Improve variable naming in factorial program

--- Factorial.java
+++ Factorial.java
@@ -1,12 +1,13 @@
 public class Factorial {
     public static void main(String[] args) {
-        int n = 5;
-        int fact = 1;
+        int number = 5;
+int factorial = 1;
 
-        for (int i = 1; i <= n; i++) {
-            fact *= i;
-        }
+for (int i = 1; i <= number; i++) {
+    factorial *= i;
+}
 
-        System.out.println("Factorial = " + fact);
+System.out.println("Factorial = " + factorial);
     }
 }
+
